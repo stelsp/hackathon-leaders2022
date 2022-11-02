31e993f0eef40f2c6fb9e020cc06c130e0beb50f
stelsp <stelspeh@gmail.com>
2022-11-03 01:39:34 +0300

    fext: header styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,69 +2,52 @@ import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const App = () => {
-  const [showed, setShowed] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const handleOpen = (e) => {
-    e.stopPropagation();
-    setShowed(true);
-  };
-
-  const handleClose = (e) => {
-    e.stopPropagation();
-    setShowed(false);
+  const handleToggle = () => {
+    setShow((show) => !show);
   };
 
   return (
-    <div className="flex flex-col">
-      <header
-        onClick={handleClose}
-        className="relative min-h-[3rem] px-2 flex items-center justify-end bg-gray-100"
-      >
-        <div className="flex items-center gap-8">
-          <p>Header</p>
-          <div className="w-8 h-8 p-2 flex items-center justify-center text-gray-100 text-bold text-lg bg-sky-100 rounded-full hover:scale-105 cursor-pointer active:scale-110">
+    <div className="overflow-hidden">
+      <header className="py-4 flex items-center bg-white border-b-2 border-sky-100 ">
+        <div className="relative flex items-center justify-between container mx-auto ">
+          <nav className="flex gap-8 text-sky-100 font-bold">
+            <Link to="/comparison">Главная</Link>
+            <Link to="/product">Продукт</Link>
+          </nav>
+          <nav
+            className={
+              show
+                ? "absolute z-10 top-5 right-5 p-4 flex flex-col gap-4 rounded-md shadow-md bg-sky-200 text-gray-100 font-bold transition ease-in-out delay-150 animate"
+                : "absolute z-10 top-5 right-5  flex flex-col gap-4 rounded-md shadow-md bg-sky-200 text-gray-100 font-bold transition ease-in-out delay-150 opacity-0 translate-x-[50%] translate-y-[-50%] w-0 h-0"
+            }
+          >
+            <Link
+              onClick={handleToggle}
+              to="/login"
+              className={!show && "hidden"}
+            >
+              Войти
+            </Link>
+            <Link
+              onClick={handleToggle}
+              to="/sign_up"
+              className={!show && "hidden"}
+            >
+              Зарегистрироваться
+            </Link>
+          </nav>
+          <div
+            onClick={handleToggle}
+            className="relative z-20 w-10 h-10 flex items-center justify-center text-gray-100 font-bold text-lg bg-sky-100 rounded-full shadow-md hover:scale-105 cursor-pointer active:scale-110"
+          >
             R
           </div>
         </div>
       </header>
-
-      {/* TODO: поправить div костыль ниже */}
-      <div onClick={handleClose}>
+      <div className="container mx-auto">
         <Outlet />
-      </div>
-
-      <aside
-        className={
-          showed
-            ? "px-11 absolute top-5 left-5 rounded-md z-10 bg-sky-200 transition ease-in-out delay-150"
-            : "px-11 absolute z-10 bg-sky-200 opacity-0 hidden transition ease-in-out delay-150"
-        }
-      >
-        <nav className="py-10 flex flex-col gap-8 text-gray-100">
-          <Link onClick={handleClose} to="/comparison">
-            Главная
-          </Link>
-          <Link onClick={handleClose} to="/product">
-            Продукт
-          </Link>
-          <Link onClick={handleClose} to="/login">
-            Войти
-          </Link>
-          <Link onClick={handleClose} to="/sign_up">
-            Зарегистрироваться
-          </Link>
-        </nav>
-      </aside>
-      <div
-        onClick={showed ? handleClose : handleOpen}
-        className="absolute z-20 top-2 left-2 w-8 h-8 p-2 flex items-center justify-center text-gray-100 text-bold text-lg bg-sky-100 rounded-full transition ease-in-out delay-150 hover:scale-105 cursor-pointer active:scale-110"
-        // className={
-        //   showed
-        //     ? "absolute z-20 top-2 left-2 w-8 h-8 p-2 flex items-center justify-center text-sky-100 text-bold text-lg bg-gray-100 rounded-full transition ease-in-out delay-150 hover:scale-105 cursor-pointer active:scale-110"
-        //     : "absolute z-20 top-2 left-2 w-8 h-8 p-2 flex items-center justify-center text-gray-100 text-bold text-lg bg-sky-100 rounded-full transition ease-in-out delay-150 hover:scale-105 cursor-pointer active:scale-110"
-        // }
-      >
-        T
       </div>
     </div>
   );
